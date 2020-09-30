b69c8c6b94d28f39b12698c205b5d7498b3bd1ad
Andrew <luiss8329@gmail.com>
2020-09-30 00:31:29 -0500

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,52 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+ 
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale || session[:locale] = I18.locale
+    #Rails.application.routes.default_url_options[:locale]= I18n.locale
+
+    if language_change_necessary?
+        I18n.locale = the_new_locale
+        set_locale_cookie(I18n.locale)
+    else
+        use_locale_from_cookie
+  	end
+  end
+
+  def default_url_options(options={})
+  	{:locale => I18n.locale}
+  end
+
+  # A locale change is necessary if no locale cookie is found, or if the locale param has been specified
+  def language_change_necessary?
+    return cookies[:locale].nil? || params[:locale]
+  end
+ # The new locale is taken from the current_user language setting, it logged_in, or from the http accept language header if not
+  # In both cases, if a locale param has been passed, it takes precedence. Only available locales are accepted
+  def the_new_locale
+    new_locale = (params[:locale] || extract_locale_from_accept_language_header)
+    ['en', 'es'].include?(new_locale) ? new_locale : I18n.default_locale.to_s
+  end
+
+  # Sets the locale cookie
+  def set_locale_cookie(locale)
+    cookies[:locale] = locale.to_s
+  end
+
+  # Reads the locale cookie and sets the locale from it
+  def use_locale_from_cookie
+    I18n.locale = cookies[:locale]
+  end
+
+  # Extracts the locale from the accept language header, if found
+  def extract_locale_from_accept_language_header
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue I18n.default_locale
+  end
+
+  #sourrce https://gist.github.com/jaimeiniesta/194956
+  #https://stackoverflow.com/questions/1253468/best-approach-for-localize-images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,86 +1,102 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #namspace for staff-admins.
-  namespace :alr_admin do
+  root 'home#index', :locale => /es|en/, as: :redirected_root, via: [:get]
+  match '/home', to: 'home#index', :locale => /es|en/, via: [:get]
+  match ':locale/home', to: 'home#index', :locale => /es|en/, via: [:get]
+
+  match '/alr_admin', to: 'alr_admin/alr_admin#index', :locale => /es|en/, via: [:get]
+  match '/(:locale)/alr_admin', to: 'alr_admin/alr_admin#index', :locale => /es|en/, via: [:get]
+
+  match '/blog', to: 'blog/blog#index', :locale => /es|en/, via: [:get]
+  match '/(:locale)/blog', to: 'blog/blog#index', :locale => /es|en/, via: [:get]
+
+  match '/admin', to: 'admin/admin#index', :locale => /es|en/, via: [:get]
+  match '/(:locale)/admin', to: 'admin/admin#index', :locale => /es|en/, via: [:get]
+  
+  scope "(:locale)", :locale => /es|en/ do
+    #namspace for staff-admins.
+    namespace :alr_admin do
+    end
+
+    #namespace for blog
+    namespace :blog do
+    end
+
+    #namespace for demo admin and sandbox;
+    namespace :admin do
+    end
+
+    #routes for the demos
+
+    get '/about', to: 'home#about'
+
+    get '/widgets', to: 'home#widgets'
+    #Base CSS
+    get '/grid_system', to: 'home#grids'
+    get '/typography', to: 'home#typography'
+    get '/icons', to: 'home#icons'
+    get '/colors', to: 'home#colors'
+    get '/helpers', to: 'home#helpers'
+    get '/rtl', to: 'home#rtl'
+    #Data operative
+    get '/tables', to: 'home#tables'
+    get '/forms', to: 'home#forms'
+    get '/buttons', to: 'home#buttons'
+    get '/donut_chart', to: 'home#donut_chart'
+    get '/progressbar', to: 'home#progressbar'
+    get '/button_groups', to: 'home#button_groups'
+    get '/keypad', to: 'home#keypad'
+    #---------------------------------------------
+    get '/form_validation', to: 'home#form_validation'
+    get '/draggable', to: 'home#draggable'
+    get '/events', to: 'home#events'
+    get '/hotkeys', to: 'home#hotkeys'
+    #Visuals
+    get '/charms', to: 'home#charms'
+    get '/tiles', to: 'home#tiles'
+    get '/carousel', to: 'home#carousel'
+    get '/panels', to: 'home#panels'
+    get '/rating', to: 'home#rating'
+    get '/preloaders', to: 'home#preloaders'
+    get '/images', to: 'home#images'
+    get '/streamer', to: 'home#streamer'
+    get '/presenter', to: 'home#presenter'
+    #Menus
+    get '/appbar', to: 'home#appbar'
+    get '/regmenus', to: 'home#regmenus'
+    get '/fluentmenus', to: 'home#fluentmenus'
+    get '/sidebar', to: 'home#sidebar'
+    #Information
+    get '/notify', to: 'home#notify'
+    get '/dialogs', to: 'home#dialogs'
+    get '/windows', to: 'home#windows'
+    get '/pops', to: 'home#pops'
+    get '/hints', to: 'home#hints'
+    #Controls
+    get '/accordion', to: 'home#accordion'
+    get '/tabs', to: 'home#tabs'
+    get '/treeview', to: 'home#treeview'
+    get '/listview', to: 'home#listview'
+    get '/slider', to: 'home#slider'
+    get '/wizards', to: 'home#wizards'
+    #Date & time
+    get '/calendar', to: 'home#calendar'
+    get '/datepicker', to: 'home#datepicker'
+    get '/countdown', to: 'home#countdown'
+    #Media
+    get '/audio_player', to: 'home#audio_player'
+    get '/video_player', to: 'home#video_player'
+    #Third party
+    get '/select2', to: 'home#select2'
+    get '/data_tables', to: 'home#data_tables'
+    get '/animate', to: 'home#animate'
+    #Utils
+    get '/dark_mode', to: 'home#dark_mode'
+    get '/easing', to: 'home#easing'
+    get '/reqjs', to: 'home#reqjs'
+
+    #Templates
+    #Admin example see line 13
   end
-
-  #namespace for blog
-  namespace :blog do
-  end
-
-  #namespace for demo admin and sandbox;
-  namespace :admin do
-  end
-
-  #routes for the demos
-  root 'home#index'
-
-  get '/widgets', to: 'home#widgets'
-  #Base CSS
-  get '/grid_system', to: 'home#grids'
-  get '/typography', to: 'home#typography'
-  get '/icons', to: 'home#icons'
-  get '/colors', to: 'home#colors'
-  get '/helpers', to: 'home#helpers'
-  get '/rtl', to: 'home#rtl'
-  #Data operative
-  get '/tables', to: 'home#tables'
-  get '/forms', to: 'home#forms'
-  get '/buttons', to: 'home#buttons'
-  get '/donut_chart', to: 'home#donut_chart'
-  get '/progressbar', to: 'home#progressbar'
-  get '/button_groups', to: 'home#button_groups'
-  get '/keypad', to: 'home#keypad'
-  #---------------------------------------------
-  get '/form_validation', to: 'home#form_validation'
-  get '/draggable', to: 'home#draggable'
-  get '/events', to: 'home#events'
-  get '/hotkeys', to: 'home#hotkeys'
-  #Visuals
-  get '/charms', to: 'home#charms'
-  get '/tiles', to: 'home#tiles'
-  get '/carousel', to: 'home#carousel'
-  get '/panels', to: 'home#panels'
-  get '/rating', to: 'home#rating'
-  get '/preloaders', to: 'home#preloaders'
-  get '/images', to: 'home#images'
-  get '/streamer', to: 'home#streamer'
-  get '/presenter', to: 'home#presenter'
-  #Menus
-  get '/appbar', to: 'home#appbar'
-  get '/regmenus', to: 'home#regmenus'
-  get '/fluentmenus', to: 'home#fluentmenus'
-  get '/sidebar', to: 'home#sidebar'
-  #Information
-  get '/notify', to: 'home#notify'
-  get '/dialogs', to: 'home#dialogs'
-  get '/windows', to: 'home#windows'
-  get '/pops', to: 'home#pops'
-  get '/hints', to: 'home#hints'
-  #Controls
-  get '/accordion', to: 'home#accordion'
-  get '/tabs', to: 'home#tabs'
-  get '/treeview', to: 'home#treeview'
-  get '/listview', to: 'home#listview'
-  get '/slider', to: 'home#slider'
-  get '/wizards', to: 'home#wizards'
-  #Date & time
-  get '/calendar', to: 'home#calendar'
-  get '/datepicker', to: 'home#datepicker'
-  get '/countdown', to: 'home#countdown'
-  #Media
-  get '/audio_player', to: 'home#audio_player'
-  get '/video_player', to: 'home#video_player'
-  #Third party
-  get '/select2', to: 'home#select2'
-  get '/data_tables', to: 'home#data_tables'
-  get '/animate', to: 'home#animate'
-  #Utils
-  get '/dark_mode', to: 'home#dark_mode'
-  get '/easing', to: 'home#easing'
-  get '/reqjs', to: 'home#reqjs'
-
-  #Templates
-  #Admin example see line 13
 end
